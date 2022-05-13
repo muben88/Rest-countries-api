d803afafe6c90cb22b0dc6267644d0c12c87c236
muben88 <ga4tech@gmail.com>
2022-05-13 19:02:42 +0100

    ceate header nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { BsMoonFill, BsMoon } from "react-icons/bs";
+
 function Header() {
+  const [isDark, setIsDark] = useState(false);
+
+  const modeSwitch = () => {
+    setIsDark(!isDark);
+  };
+
   return (
-    <header>
-      <p>Where in the world?</p>
+    <header className="header">
+      <p className="title">Where in the world?</p>
+
+      {!isDark ? (
+        <div className="mode-switcher">
+          <BsMoon onClick={modeSwitch} />
+          <p>Dark Mode</p>
+        </div>
+      ) : (
+        <div className="mode-switcher">
+          <BsMoonFill onClick={modeSwitch} />
+          <p>Light Mode</p>
+        </div>
+      )}
     </header>
   );
 }
