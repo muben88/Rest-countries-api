ab1cd8243a5b2ff2151a9640600855e2f854718a
muben88 <ga4tech@gmail.com>
2022-05-15 20:17:27 +0100

    add context provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { CountryDataProvider } from "./context/countryDataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CountryDataProvider>
+      <App />
+    </CountryDataProvider>
   </React.StrictMode>
 );
