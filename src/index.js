7946b33f6f858e3ef7b03d2bf43b16014201aa3e
muben88 <ga4tech@gmail.com>
2022-05-16 19:52:48 +0100

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { CountryDataProvider } from "./context/countryDataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CountryDataProvider>
-      <App />
-    </CountryDataProvider>
+    <BrowserRouter>
+      <CountryDataProvider>
+        <App />
+      </CountryDataProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
