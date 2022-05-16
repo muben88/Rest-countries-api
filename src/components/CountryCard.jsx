95fccf2ce4d3a6d858b7f7ca03b95a40dc2cfb33
muben88 <ga4tech@gmail.com>
2022-05-16 19:55:21 +0100

    add Link to countryinfo page and err handling

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,28 +1,35 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import CountryDataContext from "../context/countryDataContext";
 
 function CountryCard() {
-  const { info, isLoading } = useContext(CountryDataContext);
+  const { info, isLoading, getCountryInfo } = useContext(CountryDataContext);
   return (
     <div className="card-container">
       {isLoading ? (
         <p>Searching...</p>
       ) : info.length > 0 ? (
         info.map((item) => (
-          <div className="card" key={item.numericCode}>
-            <div className="country-flag">
-              <img src={item.flag} alt="country flag" />
-            </div>
+          <div className="card" key={item.name}>
+            <Link to="/countryinfo">
+              <div
+                className="country-flag"
+                onClick={() => getCountryInfo(item.name)}
+              >
+                <img src={item.flag} alt="country flag" />
+              </div>
+            </Link>
+
             <div className="country-info">
               <h2>{item.name}</h2>
               <p>
-                <b>Population:</b> {item.population}
+                <b>Population:</b> {item.population.toLocaleString()}
               </p>
               <p>
-                <b>Region:</b> {item.region}
+                <b>Region:</b> {item.region ? item.region : "unknown"}
               </p>
               <p>
-                <b>Capital:</b> {item.capital}
+                <b>Capital:</b> {item.capital ? item.capital : "unknown"}
               </p>
             </div>
           </div>
